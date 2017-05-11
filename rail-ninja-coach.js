b4583524aab4d0894397daf0d3c772e097847b1c
unknown <you@example.com>
2017-05-11 19:49:20 +0400

    +choice-of-places

--- rail-ninja-coach.js
+++ rail-ninja-coach.js
@@ -2,14 +2,20 @@ $(document).ready(function() {
 
 		
 	$(".select-seat.btn-yellow").on('click',function() {
-		($(this).closest(".train-wrapper").find(".coach-classes-wrapper")).toggle();
+		($(this).closest(".train-wrapper").find(".coach-classes-wrapper")).show(); /*show window*/
+		($(this).closest(".train-wrapper").find(".select-seat.btn-yellow")).hide(); /*hide button*/
+		($(this).closest(".train-wrapper")).css("border", "1px solid #acacad"); /*border for window show*/
 	});
 
 	$("#block-userblock").on('click',function() {
-		$("#block-userblock>.block__inner").toggle();
+		$("#block-userblock>.block__inner").toggle(); /*shesterenka*/
+	});
+	$(".close").on('click',function() {
+		($(this).closest(".train-wrapper").find(".coach-classes-wrapper")).hide(); /*hide window*/
+		($(this).closest(".train-wrapper").find(".select-seat.btn-yellow")).show(); /*show button*/
+		($(this).closest(".train-wrapper")).css("border", "none");
 	});
 	
-	
 
 
-});
+}); 
